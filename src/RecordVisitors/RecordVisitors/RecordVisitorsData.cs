9cff6367bc7b9c3953dcefdaebfb52cac953d163
ignatandrei <ignatandrei@yahoo.com>
2021-05-26 08:13:28 +0300

    saving first version

---
+++ src/RecordVisitors/RecordVisitors/RecordVisitorsData.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace RecordVisitors
+{
+    public class RecordVisitorsMiddleware : IMiddleware
+    {
+        public RecordVisitorsMiddleware()
+        {
+            GetUser = cnt =>
+            {
+                string name = cnt.User?.Identity?.Name;
+                if (name != null)
+                {
+                    return new Claim("user", name);
+                }
+                return cnt.User?.Claims.FirstOrDefault();
+            };
+            RegisterInScope = (cnt, claim) => cnt.Items.Add("userClaimMiddleware", claim);
+        }
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+        {
+            var claim = GetUser(context);
+            UserComing?.Invoke(this, claim);
+            RegisterInScope?.Invoke(context, claim);
+            Database?.Invoke(claim);
+            await next(context);
+        }
+
+        private Func<HttpContext, Claim> GetUser;
+        private Action<HttpContext, Claim> RegisterInScope;
+        public event EventHandler<Claim> UserComing;
+        private Action<Claim> Database;
+
+    }
+
+    public static class EndpointRoutingRecordVisitors
+    {
+        public static IApplicationBuilder UseEndpoints(this IApplicationBuilder builder, Action<IEndpointRouteBuilder> configure)
+        {
+            return builder;
+        }
+    }
+}
