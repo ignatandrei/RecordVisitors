258dadd7c3bb541de1475fc28531ae7732f58186
ignatandrei <ignatandrei@yahoo.com>
2021-05-30 16:30:49 +0300

    without 05

--- src/RecordVisitors/setVersion.ps1
+++ src/RecordVisitors/setVersion.ps1
@@ -9,16 +9,28 @@ if ($xml.Project.PropertyGroup.Version -eq $null)
 }
 
 $xml.SelectNodes("/Project/PropertyGroup/Version")[0].InnerText = $version 
-$xml.Save($fileName)
+
+if($xml.Project.PropertyGroup.PackageVersion -ne $null){
+        Write-Host " there is a PackageVersion "
+        $xml.SelectNodes("/Project/PropertyGroup/PackageVersion")[0].InnerText = $version 
+}
+
+$utf8Bom = New-Object System.Text.UTF8Encoding($true)
+$sw = New-Object System.IO.StreamWriter($fileName, $false, $utf8Bom )
+
+$xml.Save( $sw )
+$sw.Close()
+
+
 
 #reading again
 $xml=[xml](get-content $fileName)
 Write-Host "Version of " $fileName " is " $xml.Project.PropertyGroup.Version
-
+Write-Host "Package Version of " $fileName " is " $xml.Project.PropertyGroup.PackageVersion
 
 }
 
-$version = Get-Date -Format "yyyy.MM.dd.Hmm"
+$version = Get-Date -Format "yyyy.M.d.Hmm"
 Get-ChildItem -Path .\ -Filter *.csproj -Recurse -File -Name| ForEach-Object {
 
         WriteVersion $_  $version
