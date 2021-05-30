941d3002b81283b9d5c598cff49e5898a83bf6f6
ignatandrei <ignatandrei@yahoo.com>
2021-05-30 22:45:50 +0300

    no unwanted https

--- src/RecordVisitors/SampleWeb/Startup.cs
+++ src/RecordVisitors/SampleWeb/Startup.cs
@@ -61,7 +61,7 @@ namespace SampleWeb
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "SampleWeb v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
