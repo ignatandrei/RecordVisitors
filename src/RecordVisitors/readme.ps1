4729a1dbf1c33da2144a8150bffabd647b69e79e
ignatandrei <ignatandrei@yahoo.com>
2021-05-31 20:52:51 +0300

    fix #17

--- src/RecordVisitors/readme.ps1
+++ src/RecordVisitors/readme.ps1
@@ -1,25 +1,24 @@
+$path = "../../README.md"
+$path =Resolve-Path $path 
+Write-Host $path 
 
-$data1=@"
-<!--<add name=                                />
-    <add name=                                />
-    <add name=                                />-->
-a,b,c,d
-1,2,3,4
-"@
+$fileContent = gc $path
 
-$state='content'
-$data  -split "`n" |
-ForEach-Object {
-  If ($_ -match '^<!--') {
-    $state='comment'
-    return $null  # because `continue` doesn't work in a foreach-object
-  }
-  If ($_ -match '-->$') {
-    $state='content'
-    return $null
-  }
-  If ($state -eq 'content') {
-    $_
-  }
+Write-Host $fileContent.Length
+
+For($i=0;$i -lt $fileContent.Length ; $i++){
+    $line = $fileContent[$i];
+    If ($line  -match '^<!--') {
+        $state='comment'
+        $fileContent[$i] =$null  # because `continue` doesn't work in a foreach-object
+      }
+      If ($line  -match '-->$') {
+        $state='content'
+        $fileContent[$i] =$null  
+      }
+      If ($state -eq 'comment') {
+        $fileContent[$i] =$null  
+      } 
 }
 
+$fileContent |Set-Content $path
