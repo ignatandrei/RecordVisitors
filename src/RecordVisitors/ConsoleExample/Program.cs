afcb8d18d5de55ccceb7219947fe28d63ea3ab7a
Andrei Ignat <ignatandrei@yahoo.com>
2021-10-29 18:52:51 +0300

    example and docs

---
+++ src/RecordVisitors/ConsoleExample/Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+
+namespace ConsoleExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var r = new RecognizerPlugin.RecognizePlugins();
+            foreach (var item in r.AllExtensions())
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
